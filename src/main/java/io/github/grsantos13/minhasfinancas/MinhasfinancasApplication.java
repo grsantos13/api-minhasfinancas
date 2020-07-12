780873d8c24aee43af3c370768eac21463d3b441
grsantos13 <grodrigueset@gmail.com>
2020-07-12 19:12:46 -0300

    add config de cors para realizar requests;

--- src/main/java/io/github/grsantos13/minhasfinancas/MinhasfinancasApplication.java
+++ src/main/java/io/github/grsantos13/minhasfinancas/MinhasfinancasApplication.java
@@ -3,9 +3,18 @@ package io.github.grsantos13.minhasfinancas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class MinhasfinancasApplication {
+@EnableWebMvc
+public class MinhasfinancasApplication implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MinhasfinancasApplication.class, args);
